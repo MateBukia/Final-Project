4cd00c63bdeb099983048bdf709f72e0f99621a0
MateBukia <bukiamate@gmail.com>
2023-03-22 21:51:51 +0400

    Final

--- src/main/java/Stepobject/Logedinpage_steps.java
+++ src/main/java/Stepobject/Logedinpage_steps.java
@@ -2,15 +2,18 @@ package Stepobject;
 
 import Pageobject.Logedin_page;
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selectors.byAttribute;
 import static com.codeborne.selenide.Selenide.$;
 
 public class Logedinpage_steps extends Logedin_page {
+    @Step("Scroll down to view product")
     public Logedinpage_steps logintoview(){
         myproductview.scrollIntoView(true);
         return this;
     }
+    @Step("Select firs 4 products")
     public Logedinpage_steps taketocart() {
         for (int i = 1; i < 5; i++) {
 
